1087f6b97477ca1ba540e63b89dd3e52601666c3
jblanes <blanes@eps.udg.edu>
2024-02-26 05:22:50 +0100

    Update stream.bat

--- stream.bat
+++ stream.bat
@@ -13,16 +13,20 @@
 ::
 :: - 1) Download FFMPEG.EXE for Windows from:
 ::	https://www.ffmpeg.org/download.html#build-windows
+::  Put extracted file "FFMPEG.EXE" in the folder
 ::
 :: - 2) Download Media Server (MediaMTX.EXE) from:
 ::	https://github.com/bluenviron/mediamtx/releases (Select last *Windows_amd64* and unzip)
+::  Put extracted file "MediaMTX.exe" in the folder
+::  ** RUN "MediaMTX.exe" ONCE WITH ELEVATED PERMISSIONS TO ADD A RULE IN THE FIREWALL **
 ::
-:: - 3) From the same above ZIP, extract config file: MediaMTX.yml
+:: - 3) From the same above ZIP, extract, in the same folder, config file: MediaMTX.yml
 ::	Edit this file, just appending the following text at the end:
 ::	"paths:
-::         stream:
-::            source: udp://127.0.0.1:1234"
+::     stream:
+::       source: udp://127.0.0.1:1234"
 ::
+:: 
 :: -> (OPTIONAL) you can also save resources disabling unused protocols on MediaMTX.yml, setting:
 ::    rtsp: no
 ::    rtmp: no
@@ -40,19 +44,10 @@
 ::	
 Set ROOT=%~dp0%
 ::
-:: 1) Add Firewall rule to allow MediaMTX app. This has to be executed ELEVATED:
-::
-netsh advfirewall firewall show rule name="MediaMTX"
-if errorlevel 1 goto ADDRULE
-goto CONTINUE
-:ADDRULE
-netsh advfirewall firewall add rule name="MediaMTX" dir=in action=allow program="%~dp0%mediamtx.exe" enable=yes
-:CONTINUE
-::
-:: 2) Starts Local Media Server:
+:: - Starts Local Media Server:
 start /min %ROOT%mediamtx.exe
 ::
-:: 3) Stream desktop to Local Media Server:
+:: - Stream desktop to Local Media Server:
 start /min %ROOT%ffmpeg -f gdigrab -i desktop -c:v libx264 -preset ultrafast -f mpegts udp://127.0.0.1:1234?pkt_size=1316
 ::
 :: 
@@ -62,7 +57,7 @@ start /min %ROOT%ffmpeg -f gdigrab -i desktop -c:v libx264 -preset ultrafast -f 
 ::	-> "http://TEACHER_IP:8889/stream"
 ::
 ::	** can last 10 seconds or so to begin showing content, due to buffering.
-::	** minimal delay can occur (between 0.2-1.5s) depending on number of clients connected.
+::	** minimal delay can occur (between 0.2-1.5s) depending on the number of clients connected.
 ::
 
 
